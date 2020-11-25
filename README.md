a483564dead78232ea0fb7fcb369aaa21e94a37f
oktak <oktak@users.noreply.github.com>
2020-11-25 22:37:42 +0800

    Add page 1

--- README.md
+++ README.md
@@ -51,3 +51,37 @@ text text
 
 ## Backlog
 
+### ids
+https://docs.google.com/spreadsheets/d/e/2PACX-1vSyVFAzm3IAOPh_-D0EiGmP0xmH8PF0dalnl1q3w-lIFbFBoMUz41c4H51dwqs8i9ld_js4zKKb7ek6/pub?gid=0&single=true&output=tsv
+### return
+https://docs.google.com/spreadsheets/d/e/2PACX-1vSyVFAzm3IAOPh_-D0EiGmP0xmH8PF0dalnl1q3w-lIFbFBoMUz41c4H51dwqs8i9ld_js4zKKb7ek6/pub?gid=1125952830&single=true&output=tsv
+
+
+### Python script run on hk01 data team server
+- cron job or instance every entry?
+    => show after user submit only localstorage
+- requests TSV from google spreadsheet
+- count only unique anonymous id ?
+- how to count their "latest" one only?
+- generate json
+- upload AWS S3
+
+
+-----------
+
+### P1 Plaintext 
+
+webpage content of one week
+
+
+----------
+
+### P2 18 district Vue filter
+
+
+----------
+
+### P3 JS hko calendar (D3)
+
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -40,151 +40,249 @@ function detectSource (callback) {
 
 
 
+function chartDonut (chart_id, labels, data, datalabels) {
+  var ctx = document.getElementById(chart_id).getContext('2d');
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'doughnut',
+
+    // The data for our dataset
+    data: {
+      labels: labels || ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: datalabels || 'My First dataset',
+        backgroundColor: ['rgb(255, 99, 132)', 'green'],
+        // borderColor: 'rgb(255, 99, 132)',
+        data: data || [0, 10, 5, 2, 20, 30, 45]
+      }]
+    },
+
+    // Configuration options go here
+    options: {
+      responsive: true,
+      maintainAspectRatio: true,
+    }
+  });
+}
+
+/**
+ * hk01 Tracking::detectSource
+ * @param {*} callback 
+ */
+function displayResult () {
+
+}
 
 
 // using d3 for convenience
-  var main = d3.select("main");
-  var scrolly = main.select("#scrolly");
-  var figure = scrolly.select("figure");
-  var article = scrolly.select("article");
-  var step = article.selectAll(".step");
+var main = d3.select("main");
+var scrolly = main.select("#scrolly");
+var figure = scrolly.select("figure");
+var article = scrolly.select("article");
+var step = article.selectAll(".step");
 
-  // initialize the scrollama
-  var scroller = scrollama();
+// initialize the scrollama
+var scroller = scrollama();
 
-  // generic window resize listener event
-  function handleResize() {
-    // 1. update height of step elements
-    var stepH = Math.floor(window.innerHeight * 0.75);
-    step.style("height", stepH + "px");
+// generic window resize listener event
+function handleResize() {
+  // 1. update height of step elements
+  var stepH = Math.floor(window.innerHeight * 0.75);
+  step.style("height", stepH + "px");
 
-    var figureHeight = window.innerHeight * 0.8;
-    var figureMarginTop = (window.innerHeight - figureHeight) / 2;
+  var figureHeight = window.innerHeight * 0.8;
+  var figureMarginTop = (window.innerHeight - figureHeight) / 2;
 
-    figure
-      .style("height", figureHeight + "px")
-      .style("top", figureMarginTop + "px");
+  figure
+    .style("height", figureHeight + "px")
+    .style("top", figureMarginTop + "px");
 
-    // 3. tell scrollama to update new element dimensions
-    scroller.resize();
-  }
+  // 3. tell scrollama to update new element dimensions
+  scroller.resize();
+}
 
-  // scrollama event handlers
-  function handleStepEnter(response) {
-    console.log(response);
-    // response = { element, direction, index }
+// scrollama event handlers
+function handleStepEnter(response) {
+  console.log(response);
+  // response = { element, direction, index }
 
-    // add color to current step only
-    step.classed("is-active", function(d, i) {
-      return i === response.index;
-    });
+  // add color to current step only
+  step.classed("is-active", function(d, i) {
+    return i === response.index;
+  });
 
-    // update graphic based on step
-    figure.select("p").text(response.index + 1);
+  // update graphic based on step
+  figure.select("p").text(response.index + 1);
 
-    // updade graph
-    figure.selectAll("div.graph").each(function(d, i) {
-      console.log(this,i)
-      if (i === response.index) {
-        this.classList.remove("hide")
-        this.classList.add("show")
-      } else {
-        this.classList.remove("show")
-        this.classList.add("hide")
-      }
-    });
-  }
-
-  function setupStickyfill() {
-    d3.selectAll(".sticky").each(function() {
-      Stickyfill.add(this);
-    });
-  }
-
-  function handleSubmit (e) {
-    e.preventDefault()
-
-    let data = {
-      type: "return",
-      value: getAnonymousId(),
-      q1: $('#q01').val(),
-      q2: $('input[name="q02"]:checked').val(),
-      q3: "q1",
-      q4: "q1",
-      q5: "q1",
+  // updade graph
+  figure.selectAll("div.graph").each(function(d, i) {
+    console.log(this,i)
+    if (i === response.index) {
+      this.classList.remove("hide")
+      this.classList.add("show")
+    } else {
+      this.classList.remove("show")
+      this.classList.add("hide")
     }
+  });
+}
 
-    console.log(data);
+function setupStickyfill() {
+  d3.selectAll(".sticky").each(function() {
+    Stickyfill.add(this);
+  });
+}
 
+function handleSubmit (e) {
+  e.preventDefault()
 
-    fetch(G['GAPI'], {
-      method: 'POST',
-      body: JSON.stringify(data),
-      mode: 'no-cors',
-      credentials: 'include', // include, *same-origin, omit
-      redirect: 'follow',
-      headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-      }
-    }).then(response => {
-      console.log("success:", response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-
-    return false
+  let data = {
+    type: "return",
+    value: getAnonymousId(),
+    q1: $('#q01').val(),
+    q2: $('input[name="q02"]:checked').val(),
+    q3: "q1",
+    q4: "q1",
+    q5: "q1",
   }
 
-  function init() {
-    /**
-     * Scrollma 
-     *
-    setupStickyfill();
+  console.log(data);
 
-    // 1. force a resize on load to ensure proper dimensions are sent to scrollama
-    handleResize();
 
-    // 2. setup the scroller passing options
-    // 		this will also initialize trigger observations
-    // 3. bind scrollama event handlers (this can be chained like below)
-    scroller
-      .setup({
-        step: "#scrolly article .step",
-        offset: 0.33,
-        debug: true
-      })
-      .onStepEnter(handleStepEnter);
-
-    // setup resize event
-    window.addEventListener("resize", handleResize);
-    */
-
-    let data = {
-      type: "annoymous",
-      value: getAnonymousId(),
-      url: "http://testme.com:5500/app2/index.html"
+  fetch(G['GAPI'], {
+    method: 'POST',
+    body: JSON.stringify(data),
+    mode: 'no-cors',
+    credentials: 'include', // include, *same-origin, omit
+    redirect: 'follow',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
     }
+  }).then(response => {
+    console.log("success:", response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
+  return false
+}
 
-    fetch(G['GAPI'], {
-      method: 'POST',
-      body: JSON.stringify(data),
-      mode: 'no-cors',
-      credentials: 'include', // include, *same-origin, omit
-      redirect: 'follow',
-      headers: {
-          'Content-Type': 'application/json;charset=utf-8',
-      }
-    }).then(response => {
-      console.log("success:", response);
+function init() {
+  /**
+   * Scrollma 
+   *
+  setupStickyfill();
+
+  // 1. force a resize on load to ensure proper dimensions are sent to scrollama
+  handleResize();
+
+  // 2. setup the scroller passing options
+  // 		this will also initialize trigger observations
+  // 3. bind scrollama event handlers (this can be chained like below)
+  scroller
+    .setup({
+      step: "#scrolly article .step",
+      offset: 0.33,
+      debug: true
     })
-    .catch(function (error) {
-      console.log(error);
-    });
+    .onStepEnter(handleStepEnter);
 
-    $("#butnSubmit").on("click", handleSubmit);
+  // setup resize event
+  window.addEventListener("resize", handleResize);
+  */
+
+  // GA
+  let data = {
+    type: "annoymous",
+    value: getAnonymousId(),
+    url: "http://testme.com:5500/app2/index.html"
   }
+  fetch(G['GAPI'], {
+    method: 'POST',
+    body: JSON.stringify(data),
+    mode: 'no-cors',
+    credentials: 'include', // include, *same-origin, omit
+    redirect: 'follow',
+    headers: {
+        'Content-Type': 'application/json;charset=utf-8',
+    }
+  }).then(response => {
+    console.log("success:", response);
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
 
-  // kick things off
-  detectSource(init);
+  // Load data and prepare charts
+  // fetch JSON with timesatamp
+  fetch("https://schema.org/", {
+    method: 'GET',
+    mode: 'no-cors',
+    credentials: 'include', // include, *same-origin, omit
+    redirect: 'follow',
+  }).then(response => {
+    let data = [{
+        "name": "ts",
+        "index": ["2020-11-24 00:51:19", "2020-11-23 00:54:39", "2020-11-23 00:51:19"],
+        "data": [1, 1, 1]
+    }, {
+        "name": "annoymous_id",
+        "index": ["2b27ab63-ea97-ebe7-2a75-beb220640dc6"],
+        "data": [3]
+    }, {
+        "name": "q1",
+        "index": ["26-30", "65+"],
+        "data": [2, 1]
+    }, {
+        "name": "q2",
+        "index": ["A", "B", "D"],
+        "data": [1, 1, 1]
+    }, {
+        "name": "q3",
+        "index": ["q1"],
+        "data": [3]
+    }, {
+        "name": "q4",
+        "index": ["q1"],
+        "data": [3]
+    }, {
+        "name": "q5",
+        "index": ["q1"],
+        "data": [3]
+    }, {
+        "name": "q6",
+        "index": [],
+        "data": []
+    }]
+
+    let result = {}
+    result["q1"] = data.filter(o => o.name === "q1")
+    result["q2"] = data.filter(o => o.name === "q2")
+    result["q3"] = data.filter(o => o.name === "q3")
+    result["q4"] = data.filter(o => o.name === "q4")
+    result["q5"] = data.filter(o => o.name === "q5")
+
+    console.log(result["q1"]);
+
+    if (result["q1"].length) {
+      chartDonut("chart01", result["q1"][0]["index"], result["q1"][0]["data"], "hihi")
+    }
+    if (result["q2"].length) {
+      chartDonut("chart02", result["q2"][0]["index"], result["q2"][0]["data"], "hihi")
+    }
+    if (result["q3"].length) {
+      chartDonut("chart03", result["q3"][0]["index"], result["q3"][0]["data"], "hihi")
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+  // Handlers
+  $("#butnSubmit").on("click", handleSubmit);
+}
+
+// kick things off
+detectSource(init);
